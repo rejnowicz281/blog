d9b3ddade3eeba172e8c25e19135ca201d4c65b2
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-08 18:48:02 +0200

    Utilize router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Post from "./Post/Post";
+import Posts from "./Post/Posts";
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={<Navigate to="/blog/posts" />} />
+                <Route path="/blog/posts" element={<Posts />} />
+                <Route path="/blog/posts/:id" element={<Post />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
